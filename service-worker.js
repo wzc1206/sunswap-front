cc4fbc3c89ee030d30e4a86db4318d445ac66907
chyna <chyna.qiu@tron.network>
2023-12-15 14:05:00 +0800

    btfs 1215

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.cde66bf9687728e3853841d1bc4a1ffa.js"
+  "./precache-manifest.8bed28ec118c6abc0408168191fc422d.js"
 );
 
 self.addEventListener('message', (event) => {
